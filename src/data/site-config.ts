f0f2eb64e3043d9ada1cfbdfe0377a736b5a4b1d
Allison <Allison.jpb@gmail.com>
2024-10-05 05:20:14 -0300

    customization part 1, I gotta sleep

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -7,6 +7,8 @@ export type Image = {
 export type Link = {
     text: string;
     href: string;
+    download?: boolean;
+    target?: string;
 };
 
 export type Hero = {
@@ -38,8 +40,8 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: 'Allison Peña',
+    subtitle: 'Full-stack web developer',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
@@ -47,75 +49,98 @@ const siteConfig: SiteConfig = {
     },
     headerNavLinks: [
         {
-            text: 'Home',
+            text: 'Inicio',
             href: '/'
         },
         {
-            text: 'Projects',
+            text: 'Proyectos',
             href: '/projects'
         },
         {
-            text: 'Blog',
-            href: '/blog'
-        },
-        {
-            text: 'Tags',
-            href: '/tags'
-        }
-    ],
-    footerNavLinks: [
-        {
-            text: 'About',
+            text: 'Acerca de mí',
             href: '/about'
         },
         {
-            text: 'Contact',
+            text: 'Contacto',
             href: '/contact'
         },
         {
-            text: 'Terms',
-            href: '/terms'
+            text: 'Curriculum',
+            href: '/00-CV-Allison-peña-desarrollador-fullstack.pdf',
+            download: true,
+            target: '_blank'
+        }
+        // {
+        //     text: 'Blog',
+        //     href: '/blog'
+        // },
+        // {
+        //     text: 'Tags',
+        //     href: '/tags'
+        // }
+    ],
+    footerNavLinks: [
+        {
+            text: 'Inicio',
+            href: '/'
         },
         {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
+            text: 'Proyectos',
+            href: '/projects'
+        },
+        {
+            text: 'Acerca de mí',
+            href: '/about'
+        },
+        {
+            text: 'Contacto',
+            href: '/contact'
+        },
+        {
+            text: 'Curriculum',
+            href: '/00-CV-Allison-peña-desarrollador-fullstack.pdf',
+            download: true
         }
+        // {
+        //     text: 'Terms',
+        //     href: '/terms'
+        // },
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
-        {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
-        },
-        {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
+            text: 'LinkedIn',
+            href: 'https://linkedin.com/in/skryensya'
         }
+        // {
+        //     text: 'Instagram',
+        //     href: 'https://instagram.com/Skryensya_'
+        // },
+        // {
+        //     text: 'X/Twitter',
+        //     href: 'https://twitter.com/'
+        // }
     ],
-    hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-        image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
-        },
-        actions: [
-            {
-                text: 'Get in Touch',
-                href: '/contact'
-            }
-        ]
-    },
+    // hero: {
+    //     title: '¡Hola! Bienvenido a mi sitio web',
+    //     text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+    //     image: {
+    //         src: '/hero.jpeg',
+    //         alt: 'A person sitting at a desk in front of a computer'
+    //     },
+    //     actions: [
+    //         {
+    //             text: 'Get in Touch',
+    //             href: '/contact'
+    //         }
+    //     ]
+    // },
     subscribe: {
         title: 'Subscribe to Dante Newsletter',
         text: 'One update per week. All the latest posts directly in your inbox.',
         formUrl: '#'
     },
-    postsPerPage: 8,
-    projectsPerPage: 8
+    postsPerPage: 5,
+    projectsPerPage: 5
 };
 
 export default siteConfig;
