0e4a85216a043c45daf34fb396c373e2499dbc57
Allison Peña <allison@asimov.cl>
2025-10-13 20:55:22 -0300

    v1 count visitors

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,14 @@ interface Window {
   AudioContext: typeof AudioContext;
   webkitAudioContext: typeof AudioContext;
 }
+
+interface ImportMetaEnv {
+  readonly CLOUDFLARE_WORKER_URL: string;
+  readonly PUBLIC_TURNSTILE_SITE_KEY?: string;
+  readonly TURNSTILE_SECRET_KEY?: string;
+  readonly WORKER_AUTH_TOKEN?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
